f97bfd6b99598336d65665d20e994dbd5b0951ed
sanjay <sanjudhaker5143@gmail.com>
2026-06-24 10:52:01 +0000

    Fix backend configuration

--- backend.tf
+++ backend.tf
@@ -1,5 +1,9 @@
 terraform {
   required_version = ">= 1.5.0, < 2.0.0"
 
-  backend "s3" {}
+  backend "s3" {
+    bucket = "reyaz-terraform-state-prod-9098"
+    key    = "terraform-aiops/terraform.tfstate"
+    region = "ap-south-1"
+  }
 }
